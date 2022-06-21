3246f2ab8113ff67a4ee1d88e76096c9b5d9e7a1
kenneth-nguyenn <35188010+kenneth-nguyenn@users.noreply.github.com>
2022-06-21 17:46:34 +0700

    add route: add method GET; uri: /books, /books/{id}, /books/{id}/review, /test; controller: BookController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Route::get('books/{id}/review', [BookController::class, 'review']);
+//Route::resource('/books', BookController::class);
+
+Route::controller(BookController::class)->group(function () {
+    Route::get('/books', 'index');
+    Route::get('/books/{id}', 'show');
+    Route::get('/books/{id}/review', 'review');
+    Route::get('/test', 'testQuery');
 });
